38da5636f2e509e4d5a5258c3f682a417aa1e2cb
Andrew Birt <abirt1986@gmail.com>
2021-11-17 15:45:58 -0500

    complete basic user authentication layout and refactoring

--- src/scripts/auth/LoginForm.js
+++ src/scripts/auth/LoginForm.js
@@ -1,9 +1,13 @@
 import {Nutshell} from "../Nutshell.js"
 const contentTarget = document.querySelector(".auth--login")
-const eventHub = document.querySelector(".container")
+const eventHub = document.querySelector("#container")
 
 // When the user clicks the login button
 eventHub.addEventListener("click", e => {
+    if (e.target.id === 'register-redirect') {
+        document.querySelector('.auth--register').classList.remove('d-none');
+        document.querySelector('.auth--login').classList.add('d-none');
+    }
     if (e.target.id === "login__button") {
 
         // Get their email from the login form
@@ -36,11 +40,46 @@ eventHub.addEventListener("click", e => {
 
 const render = () => {
     contentTarget.innerHTML += `
-        <section class="login">
-            <input id="login__email" type="text" placeholder="Log In With Email">
-            <button id="login__button">Log In</button>
-        </section>
-    `
+    <div class="page-header align-items-start min-vh-100 login" style="background-image: url('https://trello-backgrounds.s3.amazonaws.com/SharedBackground/2400x1600/8bf1d00ba0e9a275807575e0b1f256f2/photo-1617168007503-25db5f117fa5.jpg');" loading="lazy">
+        <span class="mask bg-gradient-dark opacity-6"></span>
+        <div class="container my-auto">
+            <div class="row">
+                <div class="col-lg-4 col-md-8 col-12 mx-auto">
+                    <div class="card z-index-0 fadeIn3 fadeInBottom">
+                        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                            <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                            <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
+                            <div class="row mt-3">
+                                <div class="col-2 text-center ms-auto"></div>
+                                <div class="col-2 text-center px-1">
+                                    <a class="btn btn-link px-3" href="javascript:;">
+                                        <i class="fa fa-github text-white text-lg"></i>
+                                    </a>
+                                </div>
+                                <div class="col-2 text-center me-auto"></div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <form role="form" class="text-start">
+                                <div class="input-group input-group-outline my-3">
+                                    <input type="email" id="login__email" class="form-control" placeholder="Email">
+                                </div>
+                                <div class="text-center">
+                                    <button type="button" id="login__button" class="btn bg-gradient-primary w-100 my-4 mb-2">Log in</button>
+                                </div>
+                                <p class="mt-4 text-sm text-center">
+                                    Don't have an account?
+                                    <a href="javascript:;" id="register-redirect" class="text-primary text-gradient font-weight-bold">Register</a>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
 }
 
 export const LoginForm = () => {
--- src/scripts/auth/RegisterForm.js
+++ src/scripts/auth/RegisterForm.js
@@ -1,10 +1,14 @@
 import { Nutshell } from "../Nutshell.js";
 
 const contentTarget = document.querySelector(".auth--register");
-const eventHub = document.querySelector(".container");
+const eventHub = document.querySelector("#container");
 
 // When the user clicks on the register button
 eventHub.addEventListener("click", (e) => {
+  if (e.target.id === 'login-redirect') {
+    document.querySelector('.auth--register').classList.add('d-none');
+    document.querySelector('.auth--login').classList.remove('d-none');
+  }
   if (e.target.id === "register--button") {
     // Get the email they typed into the form
     const email = document.querySelector("#register--email").value;
@@ -48,11 +52,45 @@ eventHub.addEventListener("click", (e) => {
 
 const render = () => {
   contentTarget.innerHTML += `
-        <section class="register">
-            <input id="register--email" type="text" placeholder="Register with email">
-
-            <button id="register--button">Register</button>
-        </section>
+    <div class="page-header align-items-start min-vh-100 register" style="background-image: url('https://trello-backgrounds.s3.amazonaws.com/SharedBackground/2400x1600/8bf1d00ba0e9a275807575e0b1f256f2/photo-1617168007503-25db5f117fa5.jpg');" loading="lazy">
+      <span class="mask bg-gradient-dark opacity-6"></span>
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-4 col-md-8 col-12 mx-auto">
+            <div class="card z-index-0 fadeIn3 fadeInBottom">
+              <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                  <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Register</h4>
+                  <div class="row mt-3">
+                    <div class="col-2 text-center ms-auto"></div>
+                    <div class="col-2 text-center px-1">
+                      <a class="btn btn-link px-3" href="javascript:;">
+                        <i class="fa fa-github text-white text-lg"></i>
+                      </a>
+                    </div>
+                    <div class="col-2 text-center me-auto"></div>
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <form role="form" class="text-start">
+                  <div class="input-group input-group-outline my-3">
+                    <input type="email" id="register--email" class="form-control" placeholder="Email">
+                  </div>
+                  <div class="text-center">
+                    <button type="button" id="register--button" class="btn bg-gradient-primary w-100 my-4 mb-2">Register</button>
+                  </div>
+                  <p class="mt-4 text-sm text-center">
+                    Have an account?
+                    <a href="javascript:;" id="login-redirect" class="text-primary text-gradient font-weight-bold">Log in</a>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     `;
 };
 

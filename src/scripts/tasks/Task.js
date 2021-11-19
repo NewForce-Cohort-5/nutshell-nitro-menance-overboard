f4a5572563a5fb0e354edba99e2762a78cf8bba3
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-19 12:06:33 -0500

    working on rendering daily task section to dashboard

--- src/scripts/tasks/Task.js
+++ src/scripts/tasks/Task.js
@@ -1,10 +1,57 @@
+// export const Task = (task) => {
+//     return `
+//         <section class="task-card">
+//             <div class="task-name">${task.name}</div>
+//             <div class="task-date">${new Date(task.date).toLocaleDateString('en-US')}</div>
+//             <div class="task-expectedFinish">${new Date(task.expectedFinish).toLocaleDateString('en-US')}</div>
+//             <div class="task-completed">${task.completed}</div>
+//         </section>
+//         `
+// }
+
 export const Task = (task) => {
     return `
-        <section class="task-card">
-            <div class="task-name">${task.name}</div>
-            <div class="task-date">${new Date(task.date).toLocaleDateString('en-US')}</div>
-            <div class="task-expectedFinish">${new Date(task.expectedFinish).toLocaleDateString('en-US')}</div>
-            <div class="task-completed">${task.completed}</div>
-        </section>
+    <tr class="task-card">
+    <td>
+      <div class="form-check form-check-inline">
+        <h6 class="mb-0 text-sm task-date">${new Date(task.date).toLocaleDateString('en-US')}</h6>
+      </div>
+    </td>
+    <td>
+      <div class="d-flex px-2 py-1">
+        <div
+          class="d-flex flex-column justify-content-center"
+        >
+          <h6 class="mb-0 text-sm task-name">${task.name}</h6>
+        </div>
+      </div>
+    </td>
+    <td>
+      <div class="form-check form-check-inline">
+        <h6 class="mb-0 text-sm task-expectedFinish">${new Date(task.expectedFinish).toLocaleDateString('en-US')}</h6>
+      </div>
+    </td>
+    
+    <td>
+      <div class="form-check form-check-inline">
+        <i class="bi bi-pencil-square m-3"></i>
+      </div>
+    </td>
+    <td>
+      <div class="form-check form-check-inline">
+        <i class="bi bi-trash m-4"></i>
+      </div>
+    </td>
+    <td>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          id=""
+          value="option1"
+        />
+      </div>
+    </td>
+  </tr>
         `
 }
--- src/scripts/tasks/TaskList.js
+++ src/scripts/tasks/TaskList.js
@@ -1,7 +1,8 @@
 import { getTasks, useTasks } from "./TaskDataProvider.js";
 import { Task } from "./Task.js";
 
-const contentTarget = document.querySelector(".task-list")
+// const contentTarget = document.querySelector(".task-list")
+const contentTarget = document.querySelector(".dashboard")
 
 export const TaskList = () => {
     getTasks()
@@ -10,9 +11,9 @@ export const TaskList = () => {
             let tasksArray = useTasks();
             let taskHTML = "";
             tasksArray.forEach(singleTask => {
-                if (singleTask.completed === false && singleTask.userId === +sessionStorage.activeUser){
+                if (singleTask.userId === +sessionStorage.activeUser){
                 taskHTML += Task(singleTask);}
             });
-            contentTarget.innerHTML = taskHTML
+            contentTarget.innerHTML += taskHTML
         });
 };

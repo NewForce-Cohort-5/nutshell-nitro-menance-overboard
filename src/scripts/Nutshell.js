a15e0d7686e28f8ad5f9f95a59e5a64886e4ce87
Andrew Birt <abirt1986@gmail.com>
2021-11-22 14:39:43 -0500

    remove message button from the ui

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -295,11 +295,6 @@ export const Nutshell = () => {
         
         <div class="card-body p-3">
           <hr class="dark horizontal" />
-          <div class="d-flex">
-            <button type="button" class="btn btn-secondary bg-gradient-secondary text-sm my-auto me-1 mb-3">
-              New Message
-             </button>
-          </div>
           <!-- New Message Input Field -->  
           <div class="input-group input-group-outline my-3">
             ${MessageForm()}
